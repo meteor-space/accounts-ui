ebc40d1d652e32ec27630c49b5fbd3303def16e6
Darko Mijić <darko.mijic@ebiz.hr>
2016-01-17 21:39:48 +0100

    Fix indentation

--- source/client/module.js
+++ source/client/module.js
@@ -19,10 +19,10 @@ Space.accountsUi = Space.Module.define('Space.accountsUi', {
   ],
 
   onInitialize() {
-  this.injector.map('SHA256').to(SHA256);
-  this.injector.map('Meteor.user').to(Meteor.user);
-  this.injector.map('Meteor.users').to(Meteor.users);
-  this.injector.map('Space.accountsUi.Signups').asStaticValue();
+    this.injector.map('SHA256').to(SHA256);
+    this.injector.map('Meteor.user').to(Meteor.user);
+    this.injector.map('Meteor.users').to(Meteor.users);
+    this.injector.map('Space.accountsUi.Signups').asStaticValue();
   }
 
 });
